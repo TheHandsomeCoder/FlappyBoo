b07dd593fd48feba802e0679020747b3775dabf6
scottomalley <scottyomalley@gmail.com>
2025-06-25 23:32:14 +0100

    Better visibility

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -5,14 +5,18 @@ class_name HUD
 @onready var score_label: Label = $ScoreLabel
 @onready var scoreboard: CanvasLayer = $Scoreboard
 
+func _ready():
+    scoreboard.visible = false
+    set_score(0)
+
 func set_score(score):
-	score_label.text = str(score)
+    score_label.text = str(score)
 
 func reset():
-	set_score(0)
-	scoreboard.visible = false
+    set_score(0)
+    scoreboard.visible = false
 
 func show_scoreboard(score, high_score):
-	scoreboard.get_node("HighScore").text = "Highscore: " + str(high_score)
-	scoreboard.get_node("CurrentScore").text = "Score: " + str(score)
-	scoreboard.visible = true
+    scoreboard.get_node("HighScore").text = "Highscore: " + str(high_score)
+    scoreboard.get_node("CurrentScore").text = "Score: " + str(score)
+    scoreboard.visible = true
