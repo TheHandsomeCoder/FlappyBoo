64d7609e0c08da765c8c3f25a68decd0520cbac5
scottomalley <scottyomalley@gmail.com>
2025-06-29 14:06:03 +0100

    Updates

--- scenes/globals/signal_bus.gd
+++ scenes/globals/signal_bus.gd
@@ -3,4 +3,3 @@ extends Node
 signal player_death
 signal bird_crashed
 signal point_scored
-
--- scenes/pipe/pipe.gd
+++ scenes/pipe/pipe.gd
@@ -3,10 +3,9 @@ extends Area2D
 class_name Pipe
 
 func _ready():
-    # Connect the body_entered signal to the player_collided function
-    connect("body_entered", Callable(self, "player_collided"))
+	# Connect the body_entered signal to the player_collided function
+	connect("body_entered", Callable(self, "player_collided"))
 
 func player_collided(body: Node2D) -> void:
 	print_debug("player collided with pipe")
 	(body as Player).die()
-
